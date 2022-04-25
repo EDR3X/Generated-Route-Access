9d1a9b2052c5d47ec4c9ee066597e6e27e218899
EDR3X <dhunganaannanta@gmail.com>
2022-04-25 12:25:17 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:bloc_generated/count_page.dart';
+import 'package:bloc_generated/cubit/counter_cubit.dart';
+import 'package:bloc_generated/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final CounterCubit _counterCubit = CounterCubit();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Bloc Named Route",
+      theme: ThemeData(primarySwatch: Colors.green),
+      routes: {
+        '/': (context) => BlocProvider.value(
+              value: _counterCubit,
+              child: const HomePage(),
+            ),
+        '/counter': (context) => BlocProvider.value(
+              value: _counterCubit,
+              child: const ShowCounter(),
+            ),
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _counterCubit.close();
+    super.dispose();
+  }
+}
